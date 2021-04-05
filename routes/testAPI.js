1f7c43aeeab1938ab231cf8b83a6a88fae663948
nishant-ranjan-1227 <nishant.ranjan.pas@gmail.com>
2021-04-06 00:16:19 +0530

    Problems sorted on basis of tags and rating

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -16,18 +16,50 @@ router.get("/", async function (req, res) {
     }
   }
 
+  let allProblemsWithTag = await fetch(url);
+  let allProblemsWithTagInJson = await allProblemsWithTag.json();
+
   let userId = "bittuBhaiya"; //get from the request
-  let urlToGetAllSubmisions = "https://codeforces.com/api/user.status?handle=Fefer_Ivan&from=1&count=10"
+  let maxRating = 1800; //get from request
+  let minRating = 1500; //get from request
+  let problemCount = 10;
 
-  await console.log(url);
+  // submissions of User
+  let urlToGetAllSubmissions =
+    "https://codeforces.com/api/user.status?handle=" +
+    userId +
+    "&from=1&count=1000";
 
-  let data = await fetch(url);
+  let submissions = await fetch(urlToGetAllSubmissions);
+  let submissionsResponse = await submissions.json();
 
-  let response = await data.json();
+  let allCorrectSubmissions = [];
+  submissionsResponse.result.forEach((item) => {
+    if (item.verdict === "OK") {
+      allCorrectSubmissions.push({
+        contestId: item.problem.contestId,
+        index: item.problem.index,
+      });
+    }
+  });
 
-  console.log(response);
+  let problemsWithGivenTagsAndRating = []; //with contain an object { constestId : Number, Index : string}
+  allProblemsWithTagInJson.result.problems.forEach((problem) => {
+    //console.log(problem);
 
-  res.send(response);
+    if (problem.rating !== undefined) {
+      if (problem.rating <= maxRating && problem.rating >= minRating) {
+        problemsWithGivenTagsAndRating.push({
+          contestId: problem.contestId,
+          index: problem.index,
+          rating: problem.rating,
+          tags: problem.tags,
+        });
+      }
+    }
+  });
+
+  res.send(problemsWithGivenTagsAndRating);
 });
 
 module.exports = router;
